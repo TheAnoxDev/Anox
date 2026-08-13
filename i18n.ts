15bb54c009f64958e438d96347928ea5ee82f78f
TheAnoxDev <anoxdev@gmail.com>
2026-08-13 21:57:28 +0330

    Fix next-intl locale typing

--- i18n.ts
+++ i18n.ts
@@ -1,11 +1,22 @@
 import { getRequestConfig } from "next-intl/server";
 
 
+const locales = ["en", "fa"];
+
+
 export default getRequestConfig(async ({ locale }) => {
 
+  const currentLocale =
+    locale && locales.includes(locale)
+      ? locale
+      : "en";
+
+
   return {
+    locale: currentLocale,
+
     messages: (
-      await import(`./messages/${locale}.json`)
+      await import(`./messages/${currentLocale}.json`)
     ).default,
   };
 
